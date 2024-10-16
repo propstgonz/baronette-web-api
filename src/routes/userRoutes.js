3b50a2cee496fa5ae1dd4595036ef23a714efd30
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-16 15:17:06 +0200

    update comments

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// POST /login - Route to handle user login
+// POST /login - Ruta para gestionar el login de usuarios
 router.post('/login', userController.loginUser);
 
 module.exports = router;
