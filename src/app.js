a623e376dfc82090e8a9946d0a956f41896370f1
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-16 15:12:43 +0200

    update comments

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
+// Middleware para parsear el contenido de los JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use the user routes
+// Rutas de usuario
 app.use('/api', userRoutes);
 
-// Start the server
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
