8eece9983cf474c42f2e5e62084b8b537c3670ca
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-16 15:21:53 +0200

    update comments

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,9 @@
 const pool = require('../config/database');
 
 /**
- * Find a user by username in the database
- * @param {string} username - The username to search for
- * @returns {Promise<object>} - Returns a promise that resolves with the user data if found
+ * Buscar usuario por username en la base de datos
+ * @param {string} username - El username a buscar
+ * @returns {Promise<object>} - Devuelve un Promise con los datos de usuario.
  */
 const findUserByUsername = async (username) => {
   try {
@@ -14,7 +14,7 @@ const findUserByUsername = async (username) => {
     if (result.rows.length > 0) {
       return result.rows[0];
     } else {
-      return null; // User not found
+      return null; // Si el usuario no se encuentra
     }
   } catch (error) {
     throw new Error('Database query error');
